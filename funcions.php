945bd65a20088c5d1114220871be73618ce65bd9
Felip Xavier Reynes <felipxavierreynes@iespaucasesnoves.cat>
2018-12-11 18:12:33 +0100

    funcions

--- funcions.php
+++ funcions.php
@@ -1,7 +1,54 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: Felip Xavier
- * Date: 10/12/2018
- * Time: 23:49
- */
+
+
+function connectDB(){
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "lokomartin82";
+    $basedades = "biblioteca";
+
+
+// Create connection
+    $mysqli = new mysqli($servername, $username, $password, $basedades);
+
+
+// Check connection
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+//echo "Connected successfully";
+
+    $mysqli->set_charset("utf8");
+
+    return $mysqli;
+}
+
+
+
+function menu_desplegable($nombre, $opdcioBuida=FALSE,$textebuit){
+    echo "<select name='$nombre'>";
+    if ($opdcioBuida){
+
+        echo"<OPTION VALUE=''> $textebuit </OPTION>";
+    }
+
+    connectDB();
+
+    $sql = "SELECT *  from NACIONALITATS";
+    $cursor = connectDB()->query($sql) or die('Error query' . $sql);
+    while ($reg = $cursor->fetch_assoc()) {
+
+        echo '<option value="'.$reg['NACIONALITAT'].'">'.$reg['NACIONALITAT'].'</option>';
+
+    }
+    echo "</select>";
+}
+
+function datanull($data){
+    if($data != ''){
+        return "'$data'";
+    } else {
+        return "NULL";
+    }
+}
